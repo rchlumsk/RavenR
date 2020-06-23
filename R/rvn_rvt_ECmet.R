e7f8ac0d8311950dc397b6bcec2837f54d43ff3c
scantle <leland@scantle.com>
2020-06-23 15:59:35 -0400

    Added rvn_rvt_ECmet_fromStns function

--- R/rvn_rvt_ECmet.R
+++ R/rvn_rvt_ECmet.R
@@ -1,7 +1,6 @@
 #' EC Climate Station File Conversion
 #'
 #' Note - this function is designated to use data from the weathercan package
-#' available on Github to pull Environment Canada data
 #'
 #' rvn_rvt_ECmet converts Environment Canada historical meteorological data for a
 #' given station into the .rvt format usable in Raven.
@@ -261,4 +260,35 @@ rvn_rvt_ECmet <-  function(metdata, prd = NULL, stnName = NULL, forcing_set = 1,
   }
 
   return(TRUE)
+}
+
+#' Generate Raven .rvt files from EC Station data
+#'
+#' @param station_tbl tibble of Station info from weathercan::stations_search(). Can also be data.frame with station_ids column.
+#' @param start Date/Character. The start date of the data in YYYY-MM-DD format (applies to all stations_ids).
+#' @param end Date/Character. The end date of the data in YYYY-MM-DD format (applies to all station_ids).
+#' @param interval Character. Interval of the data, one of "hour", "day", "month".
+#' @param forcing_set Integer. Specifies the set of forcings to print to file, see \link{rvn_rvt_ECmet} (optional)
+#' @param ... Additional arguments passed to \link{rvn_rvt_ECmet}
+#'
+#' @return None
+#' @author Leland Scantlebury
+#' @export rvn_rvt_ECmet_fromStns
+#'
+#' @examples
+#' # Built for pipes
+#' weathercan::stations_search("Waterloo", interval="day") %>%
+#'             filter(station_id %in% c(4832, 4831)) %>%
+#'             rvn_rvt_ECmet_fromStns(start="1990-10-01", end="2000-09-30", interval="day")
+rvn_rvt_ECmet_fromStns <- function(station_tbl, start, end, interval, forcing_set = 1, ...) {
+
+  #-- Get data
+  datatbl <- weathercan::weather_dl(station_ids = station_tbl$station_id,
+                                    start = start, end = end, interval = interval)
+
+  #-- TODO: Error handling if no data
+
+  #-- Run rvn_rvt_ECmet
+  rvn_rvt_ECmet(metdata = datatbl, forcing_set =  ...)
+
 }
