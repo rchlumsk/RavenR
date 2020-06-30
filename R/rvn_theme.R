b92fc48bff8c15c98a1799c149b840bd64fed734
Sarah Grass <grass@ualberta.ca>
2020-06-30 16:23:03 -0600

    basic outline for Raven ggplot theme

---
+++ R/rvn_theme.R
@@ -0,0 +1,16 @@
+#' Raven R ggplot theme
+#'
+#' rvn_annual_peak makes the general Raven R Theme for all ggplots
+#'
+#' This function sets up the default theme for all ggplots generated using a
+#' built in Raven R function. Made by adjusting the built in theme_bw()
+#'
+#'@export theme_RavenR
+
+library(ggplot2)
+
+theme_RavenR <- function(){
+  theme_bw() +
+    theme(panel.border = element_rect(color = "black", size = 2))
+}
+
