d5ce7b1d2e898036c263f706cc63e1a2c70fa74d
rchlumsk <rchlumsk@uwaterloo.ca>
2021-10-22 11:37:15 -0400

    Small fix in rvn_download in check functionality (was returning the print statement as output, changed to message).
    Added rvn_rvp_fill_template to infill rvp template files with default parameter values.
    
    Incremented to v2.1.5

--- R/rvn_download.R
+++ R/rvn_download.R
@@ -14,6 +14,11 @@
 #' Any existing Raven.exe file in your RavenR/extdata folder will be overwritten with a newly downloaded executable w
 #' with this function.
 #'
+#' The `copy_path` argument may be useful for non-Windows users that are not able to use the pre-compiled Raven downloads.
+#' If the path to an existing Raven.exe file (i.e. one compiled locally) is provided, this will copy the specified file to the
+#' appropriate location and skip the download, which will allow the user to use the locally compiled Raven.exe with the `rvn_run`
+#' function.
+#'
 #' Reinstalling the RavenR package removes the previous files in the RavenR/ folder on your system, so this command
 #' will need to be re-run if RavenR is re-installed.
 #'
@@ -22,6 +27,7 @@
 #' @param version (optional) Character: The version of Raven to be downloaded. If not provided, the latest version will be downloaded.
 #' @param NetCDF (logical) whether to download the NetCDF-enabled version of Raven (default \code{FALSE})
 #' @param check (logical) if \code{TRUE}, function will only check whether 'Raven.exe' has been downloaded to the RavenR folder
+#' @param copy_path (character) path to an existing 'Raven.exe' file. If provided, this file will be copied to the appropriate folder and the download will be skipped.
 #' @return Returns \code{TRUE} if executed successfully
 #'
 #' @seealso \code{\link{rvn_run}}
@@ -48,7 +54,7 @@
 #' @importFrom stringr str_match_all str_match
 #' @importFrom utils osVersion unzip download.file
 #' @importFrom RCurl url.exists
-rvn_download<-function(version=NA,NetCDF=FALSE,check=FALSE)
+rvn_download<-function(version=NA,NetCDF=FALSE,check=FALSE,copy_path=NULL)
 {
    . <- NULL
 
@@ -58,173 +64,190 @@ rvn_download<-function(version=NA,NetCDF=FALSE,check=FALSE)
    if (check) {
 
       if (file.exists(sprintf("%s/Raven.exe",save_path))) {
-         print(sprintf("Raven.exe found in %s", save_path))
+         message(sprintf("Raven.exe found in %s", save_path))
          return(TRUE)
       } else {
-         print(sprintf("Raven.exe NOT found in %s", save_path))
+         message(sprintf("Raven.exe NOT found in %s", save_path))
          return(FALSE)
       }
-      # stop()
    }
 
-   sysinf <- Sys.info()
-   if (!is.null(sysinf))
-   {
-      os <- sysinf["sysname"]
-      if (os == "Darwin") os <- "osx"
-   }else{
-      os <- .Platform$OS.type
-      if (grepl("^darwin", R.version$os))   os <- "osx"
-      if (grepl("linux-gnu", R.version$os)) os <- "linux"
-   }
-   platform <-  tolower(os)
-
-   url <- "http://raven.uwaterloo.ca/Downloads.html"
-   html <- paste(readLines(url), collapse="\n")
-   matched <- str_match_all(html, "<a href=\"(.*?)\"")[[1]]
-   matched<-matched[grep("a href=",matched)]
-
-   if(platform=="windows")
-   {
-      arch<-osVersion
-      if (length(grep("x64",arch))==1)
+   if (is.null(copy_path)) {
+      sysinf <- Sys.info()
+      if (!is.null(sysinf))
       {
-         versions<-matched[grep("Win64",matched)]
-         versions<-str_match(versions, "Win64_\\s*(.*?)\\s*.zip")[,2]
-         if(NetCDF)
-         {
-            versions<-versions[grep("n",versions)]
-            if(length(versions)<1) stop(paste("A Raven version supporting NetCDF on: ",arch," is not available!"))
-            versions<-gsub("[^0-9.-]", "", versions) %>%
-                                           sort(.,decreasing=TRUE) %>%
-                                           paste("v",.,"n",sep="")
-            if(!is.na(version))
-            {
-               selectedVersion<-versions[paste("v",version,"n",sep="")%>% match(.,versions)]
-               if(is.na(selectedVersion)) stop (paste("A NetCDF version",version, "is not available for",arch,". Try either of [",paste(gsub("[^0-9.-]", "", versions),collapse=' , '),"] version(s)"))
-            }else{
-               selectedVersion<-versions[1]
-            }
-         }else{
-            if(length(grep('n',versions))>0) versions<-versions[-grep('n',versions)]
-            versions<-gsub("[^0-9.-]", "", versions)
-            if(length(versions)<1) stop(paste("A Raven version on: ",arch," is not available!"))
-            versions<-versions[!duplicated(versions)]%>% sort(.,decreasing=TRUE)%>% paste("v",.,sep="")
-            if(!is.na(version))
-            {
-                selectedVersion<-versions[paste("v",version,sep="")%>% match(.,versions)]
-                if(is.na(selectedVersion)) stop (paste("Version",version, "is not available for",arch,". Try either of [",paste(gsub("[^0-9.-]", "", versions),collapse=' , '),"] version(s)"))
-            }else{
-               selectedVersion<-versions[1]
-            }
-         }
-         fileName<-paste("RavenExecutableWin64_",selectedVersion,".zip",sep="")
+         os <- sysinf["sysname"]
+         if (os == "Darwin") os <- "osx"
+      }else{
+         os <- .Platform$OS.type
+         if (grepl("^darwin", R.version$os))   os <- "osx"
+         if (grepl("linux-gnu", R.version$os)) os <- "linux"
       }
-      if (length(grep("x86",arch))==1)
+      platform <-  tolower(os)
+
+      url <- "http://raven.uwaterloo.ca/Downloads.html"
+      html <- paste(readLines(url), collapse="\n")
+      matched <- str_match_all(html, "<a href=\"(.*?)\"")[[1]]
+      matched<-matched[grep("a href=",matched)]
+
+      if(platform=="windows")
       {
-         versions<-matched[grep("Win32",matched)]
-         versions<-str_match(versions, "Win32_\\s*(.*?)\\s*.zip")[,2]
+         arch<-osVersion
+         if (length(grep("x64",arch))==1)
+         {
+            versions<-matched[grep("Win64",matched)]
+            versions<-str_match(versions, "Win64_\\s*(.*?)\\s*.zip")[,2]
+            if(NetCDF)
+            {
+               versions<-versions[grep("n",versions)]
+               if(length(versions)<1) stop(paste("A Raven version supporting NetCDF on: ",arch," is not available!"))
+               versions<-gsub("[^0-9.-]", "", versions) %>%
+                  sort(.,decreasing=TRUE) %>%
+                  paste("v",.,"n",sep="")
+               if(!is.na(version))
+               {
+                  selectedVersion<-versions[paste("v",version,"n",sep="")%>% match(.,versions)]
+                  if(is.na(selectedVersion)) stop (paste("A NetCDF version",version, "is not available for",arch,". Try either of [",paste(gsub("[^0-9.-]", "", versions),collapse=' , '),"] version(s)"))
+               }else{
+                  selectedVersion<-versions[1]
+               }
+            }else{
+               if(length(grep('n',versions))>0) versions<-versions[-grep('n',versions)]
+               versions<-gsub("[^0-9.-]", "", versions)
+               if(length(versions)<1) stop(paste("A Raven version on: ",arch," is not available!"))
+               versions<-versions[!duplicated(versions)]%>% sort(.,decreasing=TRUE)%>% paste("v",.,sep="")
+               if(!is.na(version))
+               {
+                  selectedVersion<-versions[paste("v",version,sep="")%>% match(.,versions)]
+                  if(is.na(selectedVersion)) stop (paste("Version",version, "is not available for",arch,". Try either of [",paste(gsub("[^0-9.-]", "", versions),collapse=' , '),"] version(s)"))
+               }else{
+                  selectedVersion<-versions[1]
+               }
+            }
+            fileName<-paste("RavenExecutableWin64_",selectedVersion,".zip",sep="")
+         }
+         if (length(grep("x86",arch))==1)
+         {
+            versions<-matched[grep("Win32",matched)]
+            versions<-str_match(versions, "Win32_\\s*(.*?)\\s*.zip")[,2]
+            if(NetCDF)
+            {
+               versions<-versions[grep("n",versions)]
+               if(length(versions)<1) stop(paste("A Raven version supporting NetCDF on: ",arch," is not available!"))
+               versions<-gsub("[^0-9.-]", "", versions) %>%
+                  sort(.,decreasing=TRUE) %>%
+                  paste("v",.,"n",sep="")
+               if(!is.na(version))
+               {
+                  selectedVersion<-versions[paste("v",version,"n",sep="")%>% match(.,versions)]
+                  if(is.na(selectedVersion)) stop (paste("A NetCDF version",version, "is not available for",arch,". Try either of [",paste(gsub("[^0-9.-]", "", versions),collapse=' , '),"] version(s)"))
+               }else{
+                  selectedVersion<-versions[1]
+               }
+            }else{
+               if(length(grep('n',versions))>0) versions<-versions[-grep('n',versions)]
+               versions<-gsub("[^0-9.-]", "", versions)
+               if(length(versions)<1) stop(paste("A Raven version on: ",arch," is not available!"))
+               versions<-versions[!duplicated(versions)]%>% sort(.,decreasing=TRUE)%>% paste("v",.,sep="")
+               if(!is.na(version))
+               {
+                  selectedVersion<-versions[paste("v",version,sep="")%>% match(.,versions)]
+                  if(is.na(selectedVersion)) stop (paste("Version",version, "is not available for",arch,". Try either of [",paste(gsub("[^0-9.-]", "", versions),collapse=' , '),"] version(s)"))
+               }else{
+                  selectedVersion<-versions[1]
+               }
+            }
+            fileName<-paste("RavenExecutableWin32_",selectedVersion,".zip",sep="")
+         }
+      }
+      if(platform=="osx")
+      {
+         versions<-matched[grep("MacOS",matched)]
+         versions<-str_match(versions, "MacOS_\\s*(.*?)\\s*.zip")[,2]
          if(NetCDF)
          {
             versions<-versions[grep("n",versions)]
             if(length(versions)<1) stop(paste("A Raven version supporting NetCDF on: ",arch," is not available!"))
-            versions<-gsub("[^0-9.-]", "", versions) %>%
-                                           sort(.,decreasing=TRUE) %>%
-                                           paste("v",.,"n",sep="")
+            versions<-gsub("[^0-9.-]", "", versions) %>% sort(.,decreasing=TRUE) %>% paste("v",.,"n",sep="")
             if(!is.na(version))
             {
                selectedVersion<-versions[paste("v",version,"n",sep="")%>% match(.,versions)]
-               if(is.na(selectedVersion)) stop (paste("A NetCDF version",version, "is not available for",arch,". Try either of [",paste(gsub("[^0-9.-]", "", versions),collapse=' , '),"] version(s)"))
+               if(is.na(selectedVersion)) stop (paste("A NetCDF version",version, "is not available for",platform,". Try either of [",paste(gsub("[^0-9.-]", "", versions),collapse=' , '),"] version(s)"))
             }else{
                selectedVersion<-versions[1]
             }
          }else{
             if(length(grep('n',versions))>0) versions<-versions[-grep('n',versions)]
             versions<-gsub("[^0-9.-]", "", versions)
-            if(length(versions)<1) stop(paste("A Raven version on: ",arch," is not available!"))
             versions<-versions[!duplicated(versions)]%>% sort(.,decreasing=TRUE)%>% paste("v",.,sep="")
             if(!is.na(version))
             {
                selectedVersion<-versions[paste("v",version,sep="")%>% match(.,versions)]
-               if(is.na(selectedVersion)) stop (paste("Version",version, "is not available for",arch,". Try either of [",paste(gsub("[^0-9.-]", "", versions),collapse=' , '),"] version(s)"))
+               if(is.na(selectedVersion)) stop (paste("Version",version, "is not available for",platform,". Try either of [",paste(gsub("[^0-9.-]", "", versions),collapse=' , '),"] version(s)"))
             }else{
                selectedVersion<-versions[1]
             }
          }
-         fileName<-paste("RavenExecutableWin32_",selectedVersion,".zip",sep="")
+         fileName<-paste("RavenExecutableMacOS_",selectedVersion,".zip",sep="")
       }
-   }
-   if(platform=="osx")
-   {
-      versions<-matched[grep("MacOS",matched)]
-      versions<-str_match(versions, "MacOS_\\s*(.*?)\\s*.zip")[,2]
-      if(NetCDF)
+      if(platform=="linux")
       {
-         versions<-versions[grep("n",versions)]
-         if(length(versions)<1) stop(paste("A Raven version supporting NetCDF on: ",arch," is not available!"))
-         versions<-gsub("[^0-9.-]", "", versions) %>% sort(.,decreasing=TRUE) %>% paste("v",.,"n",sep="")
-         if(!is.na(version))
+         versions<-matched[grep("Linux",matched)]
+         versions<-str_match(versions, "Linux_\\s*(.*?)\\s*.zip")[,2]
+         if(NetCDF)
          {
-            selectedVersion<-versions[paste("v",version,"n",sep="")%>% match(.,versions)]
-            if(is.na(selectedVersion)) stop (paste("A NetCDF version",version, "is not available for",platform,". Try either of [",paste(gsub("[^0-9.-]", "", versions),collapse=' , '),"] version(s)"))
+            versions<-versions[grep("n",versions)]
+            if(length(versions)<1) stop(paste("A Raven version supporting NetCDF on: ",platform," is not available!"))
+            versions<-gsub("[^0-9.-]", "", versions) %>% sort(.,decreasing=TRUE) %>% paste("v",.,"n",sep="")
+            if(!is.na(version))
+            {
+               selectedVersion<-versions[paste("v",version,"n",sep="")%>% match(.,versions)]
+               if(is.na(selectedVersion)) stop (paste("A NetCDF version",version, "is not available for",platform,". Try either of [",paste(gsub("[^0-9.-]", "", versions),collapse=' , '),"] version(s)"))
+            }else{
+               selectedVersion<-versions[1]
+            }
          }else{
-            selectedVersion<-versions[1]
+            if(length(grep('n',versions))>0) versions<-versions[-grep('n',versions)]
+            versions<-gsub("[^0-9.-]", "", versions)
+            versions<-versions[!duplicated(versions)]%>% sort(.,decreasing=TRUE)%>% paste("v",.,sep="")
+            if(!is.na(version))
+            {
+               selectedVersion<-versions[paste("v",version,sep="")%>% match(.,versions)]
+               if(is.na(selectedVersion)) stop (paste("Version",version, "is not available for",platform,". Try either of [",paste(gsub("[^0-9.-]", "", versions),collapse=' , '),"] version(s)"))
+            }else{
+               selectedVersion<-versions[1]
+            }
          }
-      }else{
-         if(length(grep('n',versions))>0) versions<-versions[-grep('n',versions)]
-         versions<-gsub("[^0-9.-]", "", versions)
-         versions<-versions[!duplicated(versions)]%>% sort(.,decreasing=TRUE)%>% paste("v",.,sep="")
-         if(!is.na(version))
-         {
-            selectedVersion<-versions[paste("v",version,sep="")%>% match(.,versions)]
-            if(is.na(selectedVersion)) stop (paste("Version",version, "is not available for",platform,". Try either of [",paste(gsub("[^0-9.-]", "", versions),collapse=' , '),"] version(s)"))
-         }else{
-            selectedVersion<-versions[1]
-         }
+         fileName<-paste("RavenExecutableLinux_",selectedVersion,".zip",sep="")
       }
-      fileName<-paste("RavenExecutableMacOS_",selectedVersion,".zip",sep="")
-   }
-   if(platform=="linux")
-   {
-      versions<-matched[grep("Linux",matched)]
-      versions<-str_match(versions, "Linux_\\s*(.*?)\\s*.zip")[,2]
-      if(NetCDF)
-      {
-         versions<-versions[grep("n",versions)]
-         if(length(versions)<1) stop(paste("A Raven version supporting NetCDF on: ",platform," is not available!"))
-         versions<-gsub("[^0-9.-]", "", versions) %>% sort(.,decreasing=TRUE) %>% paste("v",.,"n",sep="")
-         if(!is.na(version))
-         {
-            selectedVersion<-versions[paste("v",version,"n",sep="")%>% match(.,versions)]
-            if(is.na(selectedVersion)) stop (paste("A NetCDF version",version, "is not available for",platform,". Try either of [",paste(gsub("[^0-9.-]", "", versions),collapse=' , '),"] version(s)"))
-         }else{
-            selectedVersion<-versions[1]
-         }
-      }else{
-         if(length(grep('n',versions))>0) versions<-versions[-grep('n',versions)]
-         versions<-gsub("[^0-9.-]", "", versions)
-         versions<-versions[!duplicated(versions)]%>% sort(.,decreasing=TRUE)%>% paste("v",.,sep="")
-         if(!is.na(version))
-         {
-            selectedVersion<-versions[paste("v",version,sep="")%>% match(.,versions)]
-            if(is.na(selectedVersion)) stop (paste("Version",version, "is not available for",platform,". Try either of [",paste(gsub("[^0-9.-]", "", versions),collapse=' , '),"] version(s)"))
-         }else{
-            selectedVersion<-versions[1]
-         }
-      }
-      fileName<-paste("RavenExecutableLinux_",selectedVersion,".zip",sep="")
-   }
-   downloadLink<-paste("http://raven.uwaterloo.ca/files/",gsub("n","",selectedVersion),"/",fileName,sep="")
-   if(!RCurl::url.exists(downloadLink)) stop("Bad file url. Check the repository!")
+      downloadLink<-paste("http://raven.uwaterloo.ca/files/",gsub("n","",selectedVersion),"/",fileName,sep="")
+      if(!RCurl::url.exists(downloadLink)) stop("Bad file url. Check the repository!")
 
-   # save to download_path (from tempdir()), then copy to RavenR/extdata folder
-   download.file(downloadLink,paste(download_path,"/",fileName,sep=""))
-   unzip(zipfile=paste(download_path,"/",fileName,sep=""),exdir=download_path)
-   file.remove(paste(download_path,"/",fileName,sep=""))
-   file.remove(paste(download_path,"/","RavenManual","_",gsub("n","",selectedVersion),".pdf",sep=""))
-   res <- file.copy(from=paste(download_path,"/","Raven.exe",sep=""),
-             to=paste(save_path,"/","Raven.exe",sep=""),
-             overwrite=TRUE)
+      # save to download_path (from tempdir()), then copy to RavenR/extdata folder
+      download.file(downloadLink,paste(download_path,"/",fileName,sep=""))
+      unzip(zipfile=paste(download_path,"/",fileName,sep=""),exdir=download_path)
+      file.remove(paste(download_path,"/",fileName,sep=""))
+      file.remove(paste(download_path,"/","RavenManual","_",gsub("n","",selectedVersion),".pdf",sep=""))
+      res <- file.copy(from=paste(download_path,"/","Raven.exe",sep=""),
+                       to=paste(save_path,"/","Raven.exe",sep=""),
+                       overwrite=TRUE)
+   } else {
+
+      ## copy local provided file path, do not download Raven.exe
+
+      if (!file.exists(copy_path) ) {
+         stop("copy_path is not a valid file, please check")
+      } else {
+
+         if (rvn_substrRight(copy_path,9) != "Raven.exe") {
+            warning("expecting a valid Raven.exe file, file will be renamed to Raven.exe when copied to local RavenR folder.")
+         }
+
+         res <- file.copy(from=copy_path,
+                   to=paste(save_path,"/","Raven.exe",sep=""),
+                   overwrite-TRUE)
+      }
+   }
 
    return(res)
 }
